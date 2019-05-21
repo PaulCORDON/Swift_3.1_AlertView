83900fcaee8218b7cf849a3401974f296d62433b
Paul <paul.cordon@laposte.net>
2019-05-21 10:22:35 +0200

    init

--- Alert View/ViewController.swift
+++ Alert View/ViewController.swift
@@ -15,6 +15,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func AlertBtnClick(_ sender: Any) {
+        /*on créer le corps de l'alerte*/
+        let alert = UIAlertController(title: "Error", message: "This is an Alert", preferredStyle: UIAlertController.Style.alert)
+        /*on créer une action ici c'est un btn pour quitter l'alerte*/
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+        /*on ajoute le button à l'alerte*/
+        alert.addAction(okButton)
+        /*on affiche l'alerte*/
+        self.present(alert,animated: true,completion: nil)
+    }
+    
 }
 
